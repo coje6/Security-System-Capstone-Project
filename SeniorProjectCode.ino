22ac84560cb917070d4a3920240fcf77ccd238b3
coje6 <75091764+coje6@users.noreply.github.com>
2021-04-03 09:08:39 -0400

    Update SeniorProjectCode.ino
    fixed a couple issues

--- SeniorProjectCode.ino
+++ SeniorProjectCode.ino
@@ -2,27 +2,19 @@
 #define PWMOutputPin 9
 #define ledPin 14
 
-#define SpeedOfSound 1125
+#define SpeedOfSound 1100
 #define LEDon 1
 #define LEDoff 0
 
 //sets the definiton of a signal recieved to be one or zero
-#define InputSignalPolarity 0
+#define InputSignalPolarity 1
 //sets the distance the the program is looking for
 #define DesiredDistance 11
 //sets the tolerance of the distance being looked for
 #define ToleranceDistance 1
 
-long int starttime;
-long int stoptime;
-long int signaltime;
-float seconds;
-float distance;
+//this variable needs to be global to hold values through iterations of the main loop
 float DistanceArray[10];
-float sum;
-float AverageDistance;
-int NoSignal;
-int i;
 
 void setup() {
   
@@ -34,17 +26,28 @@ void setup() {
 
 void loop() {
 
+    //initializes non global variables
+    int i;
+    int NoSignal;
+    long int starttime;
+    long int stoptime;
+    long int signaltime; 
+    float seconds;
+    float distance;
+    float sum;
+    float AverageDistance;
+
     //takes a starting time
     starttime=micros();
     
-    //generates a PWM output at pin 9 of 40kHz for 2 milliseconds
-    tone(PWMOutputPin, 40000, 2);
-    delay(2); //waits for the signal to be done to make sure it isn't read
+    //generates a PWM output at pin 9 of 40kHz for 1 milliseconds
+    tone(PWMOutputPin, 40000, 1);
+    delay(1); //waits for the signal to be done to make sure it isn't read
 
-    //waits until the receiver pick up a signal, takes the time
+    //waits until the receiver picks up a signal, records the time
     while(digitalRead(RecieverInputPin)==InputSignalPolarity) {
       //makes sure the code doesn't sit in this loop forever
-      if(starttime-micros() > 4000) {
+      if(starttime-micros() > 38000) { //38000 corresponds to a distance read of over 20 feet
         NoSignal=1;
         break;
       }
@@ -87,9 +90,6 @@ void loop() {
     //waits 5 milliseconds between samples, allows the residual noise from the first
     //pulse to die out
     delay(5);
-  
-    //resets the NoSignal and sum values
-    NoSignal=0;
-    sum=0;
+    
 
 }
